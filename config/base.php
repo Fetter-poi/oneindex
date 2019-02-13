70bdc4dd79300f57890b423a2ea39140d83a5139
Fetter-poi <oox_x44@163.com>
2019-02-14 00:39:24 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,105 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'material',
+  'onedrive_root' => '',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => '',
+    ),
+    'video' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'prgvnEMID65@dgFYP558=*}',
+  'client_id' => '2be1c24b-7b06-4f35-a2fe-34bf21ff0557',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAACEfexXxjamQb3OeGQ4GugvNGrc1u3LRmGUi8dZ3JSLwGZOMpEFgWQmhLZ3Jnf7Wr40X5JwioJ2BcfFh-eOtU_WpE62o2IOzXmiJP3o_Ev94qsdrRCVgyqNhPjtIjHrQP5Qp3Z3YDJ6GZZzRyoTVitwr7JNxMQR91L3AVWDG7fuFkvfhtjAiwwvRSkJ-UfZY7oYcnwb6z2Hsk1xGrCklAkkZlCpC2HbxyaQSQ-Uu4feJRxJUbtuK3kx02bfauzW9G9C_hVyq9wLmOn6i7txQkdefJOdkh7iplMRda7o1BB2qIH3tq_rIClNhW3_m329OsIbuulFpvV-RhnW9n_IRcZeWNOfeD6x8YOZzpQYGMeUbkYkUN5lZRNZgPDjO80YYw0jKppDHlq1GZa-qqPM1cUfblTYlya_ugsfvV8ndyB16JySfECNCfWECoFMra3jCEg_cqjJHjkimEWzg674cjc9Ob35Zz-5Jt3Trd_LGERJH4xfKV9QLFVLToR0RynMh0tq1bAiadjZNAabunWZGMiC44oGiIQzvBy_MQK7SgndOzy-llZIstuQ4bD1kBlnPNIKA1rRAC4jFabT2tAjuIhqUN2K3hEqCq7BL0xstPs1-Z9ok-qBenbnc8uVgqKMninkYouDbAxQSkEfTIZnG8cUrHQNcjiEzLnpwdZNiQitE9xNK6T91vjQ_3LNEo94zbPcMfQkgl3_83krGScSNh9daxcB9gOqhhpPHN7pFSE3Pu47rtVqpYds0rhiJzC6ZQPGqplL6YfKzooqRJd1hTFKdSNM8FfI9ahJlzaoeGVEPSAA',
+);
